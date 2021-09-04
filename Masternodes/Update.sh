f0f584a2894bce7de5eda50269bdbef69320128b
CryptoCows <90079184+CryptoCows@users.noreply.github.com>
2021-09-04 13:38:20 +1000

    Updated masternode update script
    Added two variables due to GitHub having them both referenced  
    VERSION=1.1.1.0
    VERSION1=1.1.1

--- Masternodes/Update.sh
+++ Masternodes/Update.sh
@@ -1,21 +1,22 @@
 #!/bin/sh
-VERSION=1.0.9.0
+VERSION=1.1.1.0
+VERSION1=1.1.1
 
 clear
 echo "Starting DAPS Masternode update script"
 echo "Stopping dapscoind..."
 dapscoin-cli stop
 echo "Downloading update..."
-wget -N https://github.com/DAPSCoin/DAPSCoin/releases/download/$VERSION/dapscoin-v$VERSION-linux.zip
+wget -N https://github.com/DAPSCoin/DAPSCoin/releases/download/$VERSION/dapscoin-v$VERSION1-x86_64-linux.zip
 echo "Installing unzip..."
 sudo apt-get install unzip -y
 echo "Extracting update..."
-sudo unzip -jo dapscoin-v$VERSION-linux.zip -d /usr/local/bin
+sudo unzip -jo dapscoin-v$VERSION1-x86_64-linux.zip -d /usr/local/bin
 echo "Set permissions on files..."
 sudo chmod +x /usr/local/bin/daps*
 echo "Running dapscoind..."
 dapscoind -daemon
 echo "Cleaning up..."
 cd
-rm -rf dapscoin-v$VERSION-linux.zip
+rm -rf dapscoin-v$VERSION1-x86_64-linux.zip
 echo "Update completed!"
